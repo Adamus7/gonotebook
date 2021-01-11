73ba77e1fd3047bec1d12e7e02558c8faf274fa8
Adamus7 <adamus_7@outlook.com>
2021-01-11 23:10:40 +0800

    Add GoRoutines

---
+++ Fundamentals/012-Goroutines/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func f(from string) {
+	for i := 0; i < 100; i++ {
+		fmt.Println(from, ":", i)
+	}
+}
+
+func main() {
+
+	f("direct")
+
+	go f("goroutine")
+
+	go func(msg string) {
+		// fmt.Println(msg)
+		for i := 0; i < 100; i++ {
+			fmt.Println(msg, ":", i)
+		}
+	}("going")
+
+	time.Sleep(time.Second)
+	fmt.Println("done")
+}
